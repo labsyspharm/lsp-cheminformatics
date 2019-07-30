40534486116bc3741a7fe48cc0833fd17538c67f
Clemens Hug <clemens.hug@gmail.com>
2019-07-30 18:18:59 -0400

    Add id conversion to tautomer server:

--- id_mapping/tautomer_server.py
+++ id_mapping/tautomer_server.py
@@ -16,6 +16,12 @@ identifier_mol_mapping = {
     "inchi": inchi.MolFromInchi,
 }
 
+mol_identifier_mapping = {
+    "smiles": Chem.MolToSmiles,
+    "inchi": inchi.MolToInchi,
+    "inchi_key": inchi.MolToInchiKey,
+}
+
 app = Flask(__name__)
 
 @app.route("/query/tautomers", methods=["POST"])
@@ -28,11 +34,11 @@ def process_tautomers():
         )
     id_used = ids_used[0]
     mol_input = request.form[id_used]
+    print(f"Requested tautomers for {id_used} {mol_input}")
     mol = identifier_mol_mapping[id_used](mol_input)
     mol_smiles = Chem.MolToSmiles(mol)
     mol_inchi = inchi.MolToInchi(mol)
     mol_inchi_key = inchi.MolToInchiKey(mol)
-    print(f"Requested tautomers for {id_used} {mol_input}")
     max_tautomers = request.form.get("tautomers", 10)
     tauts = make_tautomers(mol, max_tautomers = max_tautomers)
     print(f"Found tautomers: {len(tauts)}")
@@ -50,4 +56,27 @@ def process_tautomers():
             }
             for t in tauts
         ],
+    }
+    
+@app.route("/query/convert", methods=["POST"])
+def convert_ids():
+    format_in = request.form["in"]
+    if format_in not in identifier_mol_mapping:
+        raise ValueError("input must be one of: ", repr(list(identifier_mol_mapping.keys)))
+    format_out = request.form["out"]
+    if format_out not in mol_identifier_mapping:
+        raise ValueError("output must be one of: ", repr(list(mol_identifier_mapping.keys)))
+    id_in = request.form["value"]
+    print(f"Requested conversion of {format_in} {id_in} to {format_out}")
+    mol = identifier_mol_mapping[format_in](id_in)
+    mol_out = mol_identifier_mapping[format_out](mol)
+    return {
+        "request": {
+            "in": format_in,
+            "out": format_out,
+            "value": id_in,
+        },
+        "converted": {
+            format_out: mol_out,
+        },
     }
