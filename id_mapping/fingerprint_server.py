93a3f4faa8df16b2607780360a6892d2c6e0852b
Clemens Hug <clemens.hug@gmail.com>
2019-07-31 17:35:31 -0400

    Fingerprint server can make fingerprint db

--- id_mapping/fingerprint_server.py
+++ id_mapping/fingerprint_server.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import argparse
 import pickle
+import os
 import sys
 import gzip
 import tempfile
@@ -15,29 +17,33 @@ identifier_mol_mapping = {"smiles": Chem.MolFromSmiles, "inchi": inchi.MolFromIn
 
 app = Flask(__name__)
 
-@app.route("/fingerprints/fingerprint_db")
+@app.route("/fingerprints/fingerprint_db", methods=["POST"])
 def fingerprint_arena():
     cmpd_json = request.json["compounds"]
     cmpd_encoding = request.json["request"]["encoding"]
-    input_df = pd.read_json(cmpd_json)
+    input_df = pd.DataFrame(cmpd_json)
     print(input_df.head())
     mol_func = identifier_mol_mapping[cmpd_encoding]
-    temp_sdf = tempfile.NamedTemporaryFile(mode="wb", suffix=".sdf.gz")
+    temp_sdf = tempfile.NamedTemporaryFile(mode="wb", suffix=".sdf.gz", delete=False)
     temp_sdf_gz = gzip.GzipFile(fileobj=temp_sdf)
     sdf_writer = Chem.SDWriter(temp_sdf_gz)
-    for cmpd in input_df.iterrows():
-      m = mol_func(cmpd["compound"])
-      m.SetProp("name", cmpd["name"])
+    for cmpd in input_df.itertuples():
+      m = mol_func(str(cmpd.compound))
+      m.SetProp("name", str(cmpd.name))
       sdf_writer.write(m)
-    print("Wrote molecules to sdf")
+    sdf_writer.close()
+    temp_sdf_gz.close()
+    temp_sdf.close()
+    print("Wrote molecules to sdf", temp_sdf.name)
     temp_fps = tempfile.mkstemp(suffix=".fps")
     rdkit2fps.main(
       [
-        temp_sdf.name,
-        "--id-tag", "name",
         "-o", temp_fps[1],
+        "--id-tag", "name",
+        temp_sdf.name,
       ]
     )
+    os.remove(temp_sdf.name)
     return send_file(
       temp_fps[1]
     )
