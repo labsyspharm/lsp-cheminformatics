9715501195f837991419c1e904df77b014c926fd
Clemens Hug <clemens.hug@gmail.com>
2019-12-23 15:13:12 -0500

    Restructure repo so that the LSP cheminformatics server and the R client are separate top-level projects

--- data_processing/06_calculating_tas_similarity_processing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -t 0-2:00                        # Runtime in D-HH:MM format
-#SBATCH -p short                           # Partition to run in
-#SBATCH --mem=2G                          # Memory total in MB (for all cores)
-
-set -eux
-
-Rscript 06_calculating_tas_similarity_processing.R "$1" "$2" "$3"
