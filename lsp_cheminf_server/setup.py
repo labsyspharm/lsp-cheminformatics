cea8c73052b2df9f859da8150de96b8157cdb3e8
Clemens Hug <clemens.hug@gmail.com>
2020-01-03 14:38:10 -0500

    Increment version to v0.2.2

--- lsp_cheminf_server/setup.py
+++ lsp_cheminf_server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="lspcheminf",
-    version="0.2.1",
+    version="0.2.2",
     packages=["lspcheminf"],
     include_package_data=True,
     install_requires=[
