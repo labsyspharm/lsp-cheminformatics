cbd5311534168717352c3568de584e58d2410019
Clemens Hug <clemens.hug@gmail.com>
2020-01-10 15:31:08 -0500

    Fix draw examples

--- lsp_cheminf_rclient/R/draw.R
+++ lsp_cheminf_rclient/R/draw.R
@@ -13,8 +13,8 @@
 #'     "tofacitnib" = "InChI=1S/C16H20N6O/c1-11-5-8-22(14(23)3-6-17)9-13(11)21(2)16-12-4-7-18-15(12)19-10-20-16/h4,7,10-11,13H,3,5,8-9H2,1-2H3,(H,18,19,20)/t11-,13+/m1/s1",
 #'     "aspirin" = "InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)"
 #'   ),
-#'   file = "test.svg"
-#'   draw_args = list(molsPerRow = 6),
+#'   file = "test.svg",
+#'   draw_args = list(molsPerRow = 6)
 #' )
 #'
 #' draw_compound_grid(
@@ -22,10 +22,10 @@
 #'     "tofacitnib" = "InChI=1S/C16H20N6O/c1-11-5-8-22(14(23)3-6-17)9-13(11)21(2)16-12-4-7-18-15(12)19-10-20-16/h4,7,10-11,13H,3,5,8-9H2,1-2H3,(H,18,19,20)/t11-,13+/m1/s1",
 #'     "aspirin" = "InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)"
 #'   ),
-#'   file = "test.svg"
+#'   file = "test.svg",
 #'   common_core = "[H]N(C)C",
 #'   common_core_identifier = "smiles",
-#'   draw_args = list(molsPerRow = 6),
+#'   draw_args = list(molsPerRow = 6)
 #' )
 #' @export
 draw_compound_grid <- function(
