cb6f5822ee0997b2ec60ec796de6840f2463e52c
Clemens Hug <clemens.hug@gmail.com>
2020-01-31 09:41:34 -0500

    Bump R client and server version to 0.3

--- lsp_cheminf_server/setup.py
+++ lsp_cheminf_server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="lspcheminf",
-    version="0.2.3",
+    version="0.3",
     packages=["lspcheminf"],
     include_package_data=True,
     install_requires=[
